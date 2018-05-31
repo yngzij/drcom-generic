8de2b398ce60de346f533a9e475d063e32a85da9
yngzij <240356965@qq.com>
2018-05-31 17:56:58 +0800

    go

--- custom/ppp_esc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cp /lib/netifd/proto/ppp.sh /lib/netifd/proto/ppp.sh_bak
-sed -i '/proto_run_command/i username=`echo -e "$username"`' /lib/netifd/proto/ppp.sh
-sed -i '/proto_run_command/i password=`echo -e "$password"`' /lib/netifd/proto/ppp.sh
